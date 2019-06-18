f664dcf8051d3a07c8dacf84ddc66822dc0fb782
bilal-tragate <bilal@tragate.com>
2019-06-18 18:18:12 +0300

    dockerized vault has been fixed

--- docker/serviceA/setup.sh
+++ docker/serviceA/setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-while [ `curl -s -o /dev/null -w "%{http_code}" http://vault:8200` != 200 ]; do
+while [ `curl -s -o /dev/null -w "%{http_code}" http://vault:8200/` != 307 ]; do
     echo "waiting for enable vault"
     sleep 1
 done
 
+curl -XPOST --header "X-Vault-Token: my-root"  'http://vault:8200/v1/sys/mounts/kv' -d' {"path" :"kv","type":"kv"}'
 dotnet serviceA.dll
